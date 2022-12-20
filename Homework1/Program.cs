39de2f0662985bb39eebe5bf7cebd14e00d0a62c
Kseniia Ovchinnikova <ksyunya-fedotova@mail.ru>
2022-12-20 15:07:18 +0300

    Task3

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -40,3 +40,17 @@ if(num3>max)
 }
 */
 
+// Задача 3. Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+/*
+Console.Write("Input a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num % 2 == 0)
+{
+    Console.WriteLine(num + " - Четное");
+}
+else
+{
+    Console.WriteLine(num + " - Нечетное");
+}
+*/
