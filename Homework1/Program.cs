7a74d7aa4ada01de29ebf407795b61ac182096b4
Kseniia Ovchinnikova <ksyunya-fedotova@mail.ru>
2022-12-20 17:49:49 +0300

    Task4

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -54,3 +54,19 @@ else
     Console.WriteLine(num + " - Нечетное");
 }
 */
+
+//Задача 4. Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+/*
+Console.Write("Input a number: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int index = 0;
+
+while(index <= N)
+{
+if (index % 2 == 0 )
+{
+Console.WriteLine(index);
+}
+index++;
+}
+*/
