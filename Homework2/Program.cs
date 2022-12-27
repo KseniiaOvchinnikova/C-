779152e6d3a56dcfa1f30e72e5d0dae70cd8f79a
Kseniia Ovchinnikova <ksyunya-fedotova@mail.ru>
2022-12-27 13:34:35 +0300

    Task1

--- Homework2/Program.cs
+++ Homework2/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿// Задача 1. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int SecondNumberOfThreeDigit(int number)
+{
+    if(number <100 || number>999)
+    {
+        Console.WriteLine($"The number{number} is not Three-digit");
+        return -1;
+    }
+    return number / 10%10;
+}
+Console.Write("Enter a three-digit number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"The second number is {SecondNumberOfThreeDigit(number)}");
