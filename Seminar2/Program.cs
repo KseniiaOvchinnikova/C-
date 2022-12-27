d1eedb458ec5a6c60236e145e6dd2f45f19f625a
Kseniia Ovchinnikova <ksyunya-fedotova@mail.ru>
2022-12-27 20:47:06 +0300

    Task1

--- Seminar2/Program.cs
+++ Seminar2/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿// Задача 1. Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+
+int CutNumber(int number)
+
+    {
+        int sot = number / 100;
+        int ed = number % 10;
+
+        int result = sot * 10 + ed;
+        return result;
+    }
+
+int randNumber = new Random().Next(100,1000);
+Console.WriteLine("Current random three-digit number is " + randNumber);
+
+int newNumber = CutNumber(randNumber);
+Console.WriteLine("New version number is " + newNumber);
+
