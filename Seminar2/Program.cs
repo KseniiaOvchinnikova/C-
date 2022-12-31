9994a7c0e52d58d32c1ebf101b5f55b24ceeda7d
Kseniia Ovchinnikova <ksyunya-fedotova@mail.ru>
2022-12-31 16:59:31 +0300

    Task2

--- Seminar2/Program.cs
+++ Seminar2/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Задача 1. Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
-
+/*
 int CutNumber(int number)
 
     {
@@ -15,4 +15,30 @@ Console.WriteLine("Current random three-digit number is " + randNumber);
 
 int newNumber = CutNumber(randNumber);
 Console.WriteLine("New version number is " + newNumber);
+*/
+
+// Задача 2. Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+int Number (int numb)
+{
+    int des = numb / 10;
+    int ed = numb % 10;
+    int max = 0;
+    if(des>ed)
+    {
+        max = des;
+    }
+    else
+    {
+        max = ed;
+    }
+    return max;
+}
+int randNumber = new Random().Next(10,100);
+Console.WriteLine($"Current random two-digit number is  {randNumber}");
+
+int result = Number(randNumber);
+Console.WriteLine($"Max number is {result}");
+
+
 
