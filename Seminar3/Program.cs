77ce232f0c1ba53a09f9f119c344e7645374bb83
Kseniia Ovchinnikova <ksyunya-fedotova@mail.ru>
2023-01-10 11:20:16 +0300

    Task1

--- Seminar3/Program.cs
+++ Seminar3/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿void GetDiapazone(int quadrant)
+{
+    if(quadrant == 1) Console.WriteLine("x > 0 and y > 0");
+    else if(quadrant == 2) Console.WriteLine("x < 0 and y > 0");
+    else if(quadrant == 3) Console.WriteLine("x < 0 and y < 0");
+    else if(quadrant == 4) Console.WriteLine("x > 0 and y < 0");
+    else Console.WriteLine("Wrong input!");
+}
+
+Console.Write("Input a number of quadrant: ");
+int quadNum = Convert.ToInt32(Console.ReadLine());
+
+GetDiapazone(quadNum);
