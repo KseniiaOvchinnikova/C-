70ae169304607c54052474a589ea56b617c9254d
Kseniia Ovchinnikova <ksyunya-fedotova@mail.ru>
2023-01-13 09:28:28 +0300

    Task1

--- Seminar4/Program.cs
+++ Seminar4/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿// Задача 1. Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+
+int GetSum(int num)
+{
+    int sum = 0;
+
+    for(int current=1;current<=num;current++)
+        sum+=current;
+
+    return sum;
+}
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int result = GetSum(a);
+Console.WriteLine(GetSum(a));
