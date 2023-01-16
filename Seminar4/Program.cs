81538719ed2d48fd7b1bbaa036cb5ce6b247a65e
Kseniia Ovchinnikova <ksyunya-fedotova@mail.ru>
2023-01-16 10:52:39 +0300

    Task2, Task3

--- Seminar4/Program.cs
+++ Seminar4/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Задача 1. Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
-
+/*
 int GetSum(int num)
 {
     int sum = 0;
@@ -15,3 +15,43 @@ int a = Convert.ToInt32(Console.ReadLine());
 
 int result = GetSum(a);
 Console.WriteLine(GetSum(a));
+*/
+
+//Задача 2. Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+/*
+int Numbers(int n)
+{
+    int count = 0;
+    while(n != 0)
+    {
+        n = n / 10;
+        count += 1;
+    }
+    return count;
+}
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int result = Numbers(a);
+Console.WriteLine(Numbers(a)); 
+*/
+
+// Задача 3. Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+/*
+int GetProd(int num)
+{
+    int prod = 0;
+
+    for(int current = 1; current <= num; current++)
+        prod = current * (current + 1);
+
+    return prod;
+}
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int result = GetProd(a);
+Console.WriteLine(GetProd(a)); 
+*/
