cc8eb1418e93666bcae24ea10ea6b2420f3f0427
Kseniia Ovchinnikova <ksyunya-fedotova@mail.ru>
2023-01-23 13:10:51 +0300

    Task4

--- Seminar6/Program.cs
+++ Seminar6/Program.cs
@@ -91,4 +91,41 @@ ShowArray(result);
 */
 
 // Задача 4. Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+/*
+int[] RandomArray (int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+    for(int i = 0; i < size; i++)
+    array[i] = new Random().Next(minValue, maxValue);
 
+    return array;
+}
+
+int[] CopyArray (int[] array, int size)
+{
+    int[]newarray = new int[size];
+    for(int i = 0; i < array.Length; i++)
+        newarray[i] = array[i];
+    return newarray;
+}
+
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    Console.Write(array[i] + " ");
+
+    Console.WriteLine();
+}
+
+Console.Write("Введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите минимальное значение элемента массива: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите максимальное значение элемента массива: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] newArray = RandomArray(n, min, max);
+ShowArray(newArray);
+int[] result = CopyArray(newArray,n);
+ShowArray(result);
+*/
