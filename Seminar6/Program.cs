9054ea3a68af24b8665511e5d259be5dbdd9b274
Kseniia Ovchinnikova <ksyunya-fedotova@mail.ru>
2023-01-23 12:43:36 +0300

    Task2,3

--- Seminar6/Program.cs
+++ Seminar6/Program.cs
@@ -1,5 +1,5 @@
 ﻿// задача1. Напишите программу, которая перевернёт одномерный массив (последний элемент будет на первом месте, а первый - на последнем и т.д.)
-
+/*
 int[] RandomArray (int size, int minValue, int maxValue)
 {
     int[] array = new int[size];
@@ -38,3 +38,57 @@ int[] newArray = RandomArray(n, min, max);
 ShowArray(newArray);
 ReverseArray(newArray);
 ShowArray(newArray);
+*/
+// Задача 2. Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник с сторонами такой длины.
+/*
+bool Tringle(int a, int b, int c)
+{
+    if(a+b>c && a+c>b && b+c>a) return true;
+    else return false;
+}
+
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+bool result = Tringle(a,b,c);
+Console.WriteLine("Ответ: " + result);
+
+
+// задача 3. Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: a и b.
+
+int[] Fibonachchi(int size, int a, int b)
+{
+    int[] array = new int[size];
+    array[0] = a;
+    array[1] = b;
+    for(int i = 2; i<size; i++)
+        array[i] = array[i-2] + array[i-1];
+
+    return array;
+}
+
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    Console.Write(array[i] + " ");
+
+    Console.WriteLine();
+}
+
+Console.Write("Введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int[] result = Fibonachchi(n,a,b);
+ShowArray(result);
+*/
+
+// Задача 4. Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+
